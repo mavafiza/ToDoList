fd0b4e006e2460f9e1dd0175506774b6729009da
mava <mavafiza@gmail.com>
2022-10-15 11:10:17 +0200

    [BCN-PT-072022] - mava-figallo

---
+++ app-mava/src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: () => import('../views/AboutView.vue')
+        },
+    ]
+})
+
+export default router
